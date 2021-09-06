cb5f497d6ab31f22c4d0d15a19de86b6bd4d1b92
Pavel Siarzhantau <Pavel.Siarzhantau@gmail.com>
2021-09-06 17:38:51 +0300

    init home page
    -made html
    -made css
    -made js

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+
+document.getElementById('close-navbar').onclick = function (){
+    document.getElementById('left-side-bar').classList.toggle('hiddenClass');
+    document.getElementById('content').classList.toggle('main-full');
+}
+
+
+
